e0cba782284668215e5b9b2c1da35a5448aedad5
oangelo <oangelo@gmail.com>
2013-03-18 12:57:01 -0300

    improving the code readability

--- dk.py
+++ dk.py
@@ -1,39 +1,28 @@
 #!/usr/bin/python2.7
 from scipy.stats import f 
-from scipy.stats import distributions
 import pylab as P
 
 def dragon_kings(data_list, n, r):
-    data = list(set(data_list))
+    data = data_list
     data.sort()
     data.reverse()
 
-    N = len(data)
-    #Spacings y_k
-    y = []
-    for k in range(0, N-1):
-        y.append(data[k] - data[k+1])
-    y.append(N-1)
+    y = [(data[k] - data[k + 1]) for k in range(n)]
+    z = [(k+1)*y[k] for k in range(n)]
 
-    z = []
-    for k in range(1, N-1):
-        z.append(k*y[k])
+    chi1 = 0.0
+    for i in range(r):
+        chi1 += z[i]
+    chi1 /= r 
 
-    #Test Statistics
-    numerator = 0
-    for i in range(0, r):
-        numerator += z[i]
-    numerator = numerator / float(r)
-
-    denominator = 0.
+    chi2 = 0.0
     for i in range(r, n):
-        denominator += z[i]
-    denominator = denominator / float(n - r)
+        chi2 += z[i]
+    chi2 /= (n - r)
 
-    T = numerator / float(denominator)
-    F = distributions.f.cdf(T, 2*r, 2*(n - r));
-    print "T", T, 2*r, 2*(n-r)
-    p = 1 - F
+    T = chi1 / chi2
+    
+    p = 1 - f.cdf(T, 2*r, 2*(n - r))
 
     return p
 
@@ -47,8 +36,11 @@ for value in lines:
 
 r = 1
 
-serie = [(n, dragon_kings(data, n, r)) for n in range(2,36)]   
+serie = [(n, dragon_kings(data, n, r)) for n in range(r + 1, 36)]   
 for i in range(0, len(serie)):
     print serie[i][0], serie[i][1]
+
+for i in range(10):
+    print "# ", data[i]
 #P.plot([i[0] for i in serie],[i[1] for i in serie], '-o')
 #P.show()
